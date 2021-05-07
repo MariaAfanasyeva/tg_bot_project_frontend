dc90195865eed61f18e08bc4408d3196154770d2
maryia <masha.afanaseva.29@mail.ru>
2021-05-07 13:41:14 +0300

    added prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
 import BotsList from "./BotsList";
 import Navbar from "./Navbar";
-import CategoryDetail from "./CategoryDetail";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/category/:id" exact component={CategoryDetail} />
-                <Route path="/" exact component={BotsList} />
-            </Switch>
-        </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/category/:id/bots" exact component={BotsList} />
+          <Route path="/" exact component={BotsList} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- frontend/src/BotsList.js
+++ frontend/src/BotsList.js
@@ -1,141 +1,215 @@
 import React, { Component } from "react";
 
 export default class BotsList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: [],
-            isSelected: false,
-            selectedItems: [],
-            prev_link: '',
-            next_link: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      isSelected: false,
+      selectedItems: [],
+      prev_link: "",
+      next_link: "",
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.nextPage = this.nextPage.bind(this);
+    this.prevPage = this.prevPage.bind(this);
+  }
+  handleClick(item, event) {
+    event.preventDefault();
+    if (this.state.selectedItems.includes(item.id)) {
+      const index = this.state.selectedItems.indexOf(item.id);
+      delete this.state.selectedItems[index];
+      this.setState((state) => {
+        return {
+          isSelected: !this.state.isSelected,
+          selectedItems: this.state.selectedItems,
         };
-
-        this.handleClick = this.handleClick.bind(this);
-        this.nextPage = this.nextPage.bind(this);
-        this.prevPage = this.prevPage.bind(this);
+      });
+    } else {
+      this.setState((state) => {
+        return {
+          isSelected: !this.state.isSelected,
+          selectedItems: [this.state.selectedItems, item.id],
+        };
+      });
     }
-    handleClick(item, event) {
-        event.preventDefault();
-        if (this.state.selectedItems.includes(item.id)) {
-            const index = this.state.selectedItems.indexOf(item.id);
-            delete this.state.selectedItems[index];
-            this.setState(state => {
-                return {
-                    isSelected: !this.state.isSelected,
-                    selectedItems: this.state.selectedItems,
-                }
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    console.log(id);
+    if (id === undefined) {
+      fetch("http://127.0.0.1:8000/api/bots")
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result.results,
+              prev_link: null,
+              next_link: result.next,
             });
-        } else {
-            this.setState(state => {
-                return {
-                    isSelected: !this.state.isSelected,
-                    selectedItems: [this.state.selectedItems, item.id],
-                }
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
             });
-        }
+          }
+        );
+    } else {
+      fetch(`http://127.0.0.1:8000/api/category/${id}/bots`)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result.results,
+              prev_link: null,
+              next_link: result.next,
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
     }
-
-
-
-
-    componentDidMount() {
-        fetch('http://127.0.0.1:8000/api').then(res => res.json()).then(
-            (result) => {
-                this.setState({
-                    isLoaded: true,
-                    items: result.results,
-                    prev_link: null,
-                    next_link: result.next
-                });
-            },
-            (error) => {
-                this.setState({
-                   isLoaded: true,
-                   error
-                });
-            }
-        )
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.id !== this.props.match.params.id) {
+      const id = this.props.match.params.id;
+      fetch(`http://127.0.0.1:8000/api/category/${id}/bots`)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              items: result.results,
+              prev_link: null,
+              next_link: result.next,
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
+            });
+          }
+        );
     }
+  }
 
-    prevPage = () => {
-        if (this.state.prev_link !== null) {
-            const link = this.state.prev_link;
-            fetch(link).then(res => res.json()).then(
-                (result) => {
-                    this.setState({
-                        items: result.results,
-                        prev_link: result.previous,
-                        next_link: result.next
-                    })
-                }
-            );
-        }
+  prevPage = () => {
+    if (this.state.prev_link !== null) {
+      const link = this.state.prev_link;
+      fetch(link)
+        .then((res) => res.json())
+        .then((result) => {
+          this.setState({
+            items: result.results,
+            prev_link: result.previous,
+            next_link: result.next,
+          });
+        });
     }
+  };
 
-    nextPage = () => {
-        if (this.state.next_link !== null) {
-            const link = this.state.next_link;
-            fetch(link).then(res => res.json()).then(
-                (result) => {
-                    this.setState({
-                        items: result.results,
-                        prev_link: result.previous,
-                        next_link: result.next
-                    })
-                }
-            );
-        }
+  nextPage = () => {
+    if (this.state.next_link !== null) {
+      const link = this.state.next_link;
+      fetch(link)
+        .then((res) => res.json())
+        .then((result) => {
+          this.setState({
+            items: result.results,
+            prev_link: result.previous,
+            next_link: result.next,
+          });
+        });
     }
+  };
 
-
-
-    render() {
-        const {error, isLoaded, items, selectedItems, prev_link, next_link} = this.state;
-        if (error) {
-            return <p>Error {error.message}</p>
-        } else if (!isLoaded) {
-            return <p>Loading...</p>
-        } else if (!selectedItems) {
-            return (
-                <div>
-                    <ul>
-                        {items.map(item =>
-                            <li key={item.id}>
-                                <a id={item.id} onClick={(event) =>
-                                    this.handleClick(item, event)}>{item.name}</a>
-                            </li>
-                        )}
-                    </ul>
-                    <div className="pagination">
-                        <button disabled={prev_link === null} onClick={this.prevPage}>Previous</button>
-                        <button disabled={next_link === null} onClick={this.nextPage}>Next</button>
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div>
-                    <ul>
-                        {items.map((item) => ( (selectedItems.includes(item.id)) ?
-                            <li id={item.name}>
-                                <a id={item.id} onClick={(event) =>
-                                    this.handleClick(item, event)}>{item.name}</a>
-                                <p>{item.description}</p>
-                            </li> : <li id={item.name}>
-                                <a id={item.id} onClick={(event) =>
-                                    this.handleClick(item, event)}>{item.name}</a>
-                            </li>
-                        ))}
-                    </ul>
-                    <div className="pagination">
-                        <button disabled={prev_link === null} onClick={this.prevPage}>Previous</button>
-                        <button disabled={next_link === null} onClick={this.nextPage}>Next</button>
-                    </div>
-                </div>
-            );
-        }
+  render() {
+    const {
+      error,
+      isLoaded,
+      items,
+      selectedItems,
+      prev_link,
+      next_link,
+    } = this.state;
+    if (error) {
+      return <p>Error {error.message}</p>;
+    } else if (!isLoaded) {
+      return <p>Loading...</p>;
+    } else if (!selectedItems) {
+      return (
+        <div>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                <a
+                  id={item.id}
+                  onClick={(event) => this.handleClick(item, event)}
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="pagination">
+            <button disabled={prev_link === null} onClick={this.prevPage}>
+              Previous
+            </button>
+            <button disabled={next_link === null} onClick={this.nextPage}>
+              Next
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <ul>
+            {items.map((item) =>
+              selectedItems.includes(item.id) ? (
+                <li id={item.name}>
+                  <a
+                    id={item.id}
+                    onClick={(event) => this.handleClick(item, event)}
+                  >
+                    {item.name}
+                  </a>
+                  <p>{item.description}</p>
+                </li>
+              ) : (
+                <li id={item.name}>
+                  <a
+                    id={item.id}
+                    onClick={(event) => this.handleClick(item, event)}
+                  >
+                    {item.name}
+                  </a>
+                </li>
+              )
+            )}
+          </ul>
+          <div className="pagination">
+            <button disabled={prev_link === null} onClick={this.prevPage}>
+              Previous
+            </button>
+            <button disabled={next_link === null} onClick={this.nextPage}>
+              Next
+            </button>
+          </div>
+        </div>
+      );
     }
+  }
 }
