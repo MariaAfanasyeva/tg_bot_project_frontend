86369bb584a808db4a3de97f2f69eb3bb17e9aac
maryia <masha.afanaseva.29@mail.ru>
2021-05-28 17:47:27 +0300

    eliminated comments related to ApiFetch.js

--- frontend/src/api/ApiFetch.js
+++ frontend/src/api/ApiFetch.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { refresh } from "./RefreshApi";
 
 export const api = (method, url, login_required, data = null) => {
-  if (localStorage.getItem("access_token") !== null) {
+  if (localStorage.getItem("access_token")) {
     const token = localStorage.getItem("access_token");
     const decodedToken = jwt.decode(token);
     let dateNow = new Date();
@@ -11,52 +11,24 @@ export const api = (method, url, login_required, data = null) => {
     }
   }
 
-  const access_token =
-    localStorage.getItem("access_token") !== null
-      ? "Bearer " + localStorage.getItem("access_token")
-      : null;
-  let options = "";
+  const access_token = localStorage.getItem("access_token")
+    ? "Bearer " + localStorage.getItem("access_token")
+    : null;
+  let options = {
+    method: method,
+    mode: "cors",
+  };
   if (login_required === true) {
-    if (method === "GET") {
-      options = {
-        method: method,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: access_token,
-        },
-        mode: "cors",
+    if (method !== "GET") {
+      options.headers = {
+        "Content-Type": "application/json",
+        Authorization: access_token,
       };
-    } else {
-      options = {
-        method: method,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: access_token,
-        },
-        mode: "cors",
-        body: JSON.stringify(data),
-      };
+      options.body = JSON.stringify(data);
     }
   } else {
-    if (method === "GET") {
-      options = {
-        method: method,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: access_token,
-        },
-        mode: "cors",
-      };
-    } else {
-      options = {
-        method: method,
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: access_token,
-        },
-        mode: "cors",
-        body: JSON.stringify(data),
-      };
+    if (method !== "GET") {
+      options.body = JSON.stringify(data);
     }
   }
   return fetch(url, options);
