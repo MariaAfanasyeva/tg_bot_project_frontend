e88a1c2e834a997d64d1ba32e15230abbaf25151
maryia <masha.afanaseva.29@mail.ru>
2021-05-31 11:42:04 +0300

    changed render element

--- frontend/src/api/ApiFetch.js
+++ frontend/src/api/ApiFetch.js
@@ -4,10 +4,14 @@ import { refresh } from "./RefreshApi";
 export const api = (method, url, login_required, data = null) => {
   if (localStorage.getItem("access_token")) {
     const token = localStorage.getItem("access_token");
-    const decodedToken = jwt.decode(token);
-    let dateNow = new Date();
-    if (decodedToken.exp < dateNow.getTime() / 1000) {
+    if (!token) {
       refresh();
+    } else {
+      const decodedToken = jwt.decode(token);
+      let dateNow = new Date();
+      if (decodedToken.exp < dateNow.getTime() / 1000) {
+        refresh();
+      }
     }
   }
 
